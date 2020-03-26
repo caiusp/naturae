4ad412f11dcdb5d70c73522ace5e94d822b06371
alesasha21 <62480648+alesasha21@users.noreply.github.com>
2020-03-26 23:37:40 +0100

    Add files via upload

---
+++ File/listaSchedeHabitat.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Lista schede habitat</title>
+    <link rel="stylesheet" type="text/css" href="css/listaschedehabitat.css">
+
+  </head>
+  <body>
+    <input type="button" onclick="location.href='Profilo.php'" value="Torna alla home"/>
+    <div id="contenitore">
+      <h1>Lista schede habitat: </h1><br>
+        <div class="testo">
+
+
+          <p>campagna</p>
+          <p>campagna</p>
+          <p>campagna</p>
+          <p>campagna</p>
+          <p>campagna</p>
+          <p>campagna</p>
+          <p>campagna</p>
+          <p>campagna</p>
+          <p>campagna</p>
+          <p>campagna</p>
+          <p>campagna</p>
+          <p>campagna</p>
+          <p>campagna</p>
+          <p>campagna</p>
+          <p>campagna</p>
+          <p>campagna</p>
+          <p>campagna</p>
+          <p>campagna</p>
+          <p>campagna</p>
+          <p>campagna</p>
+          <p>campagna</p>
+          <p>campagna</p>
+          <p>campagna</p>
+          <p>campagna</p>
+
+        </div>
+
+  </div>
+
+
+  </body>
+</html>
