4427c4a5194ff50bfee4bd2a91e8f8ba6d9a0bb6
alesasha21 <62480648+alesasha21@users.noreply.github.com>
2020-03-26 23:15:42 +0100

    Add files via upload

---
+++ File/listaSpecieVegAn.php
@@ -0,0 +1,89 @@
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Lista specie vegetali</title>
+    <link rel="stylesheet" type="text/css" href="css/listaspecieveg.css">
+
+  </head>
+  <body>
+
+    <div class="col1">
+        <input type="button" onclick="location.href='Profilo.php'" value="Torna alla home"/>
+      <div id="contenitore">
+        <h1>Lista schede Vegetali: </h1><br>
+          <div class="cont1a">
+
+
+            <p>campagna</p>
+            <p>campagna</p>
+            <p>campagna</p>
+            <p>campagna</p>
+            <p>campagna</p>
+            <p>campagna</p>
+            <p>campagna</p>
+            <p>campagna</p>
+            <p>campagna</p>
+            <p>campagna</p>
+            <p>campagna</p>
+            <p>campagna</p>
+            <p>campagna</p>
+            <p>campagna</p>
+            <p>campagna</p>
+            <p>campagna</p>
+            <p>campagna</p>
+            <p>campagna</p>
+            <p>campagna</p>
+            <p>campagna</p>
+            <p>campagna</p>
+            <p>campagna</p>
+            <p>campagna</p>
+            <p>campagna</p>
+
+          </div>
+
+    </div>
+  </div>
+
+<div class="col2">
+  <div id="contenitore">
+    <h1>Lista schede animali: </h1><br>
+
+
+
+      <div class="cont1a">
+
+        <p>campagna</p>
+        <p>campagna</p>
+        <p>campagna</p>
+        <p>campagna</p>
+        <p>campagna</p>
+        <p>campagna</p>
+        <p>campagna</p>
+        <p>campagna</p>
+        <p>campagna</p>
+        <p>campagna</p>
+        <p>campagna</p>
+        <p>campagna</p>
+        <p>campagna</p>
+        <p>campagna</p>
+        <p>campagna</p>
+        <p>campagna</p>
+        <p>campagna</p>
+        <p>campagna</p>
+        <p>campagna</p>
+        <p>campagna</p>
+        <p>campagna</p>
+        <p>campagna</p>
+        <p>campagna</p>
+        <p>campagna</p>
+
+      </div>
+
+</div>
+</div>
+
+
+
+
+  </body>
+</html>
