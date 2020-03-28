dfa71716038475ace82e92fadbff817e57a50fd5
alesasha21 <62480648+alesasha21@users.noreply.github.com>
2020-03-28 20:07:29 +0100

    Update inserisciSpecieVegetale_Server.php

--- File/inserisciSpecieVegetale_Server.php
+++ File/inserisciSpecieVegetale_Server.php
@@ -1,6 +1,19 @@
 <?php
 session_start();
 
+try {
+     $mng = new MongoDB\Driver\Manager("mongodb://localhost:27017");
+     $bulk = new MongoDB\Driver\BulkWrite();
+     if($_POST){
+       $doc = ['_id' => new MongoDB\BSON\ObjectID(),'nome'=>$_SESSION['nome'], 'timestamp'=>$_POST['timestamp'], 'nomeLatino'=>$_POST['nomelat'], 'nomeItaliano'=>$_POST['nomeita'], 'classe'=>$_POST['classe'], 'wikipedia'=>$_POST['wiki'], 'vulnerabilita'=>$_POST['vulnerabilita'], 'anno'=>$_POST['anno'], 'lunghezza'=>$_POST['lunghezza'], 'diametro'=>$_POST['diametro'], 'azione' => 'Nuova specie vegetale'];
+
+     $bulk->insert($doc);
+     $mng->executeBulkWrite('Naturae.nat', $bulk);
+   }
+     } catch (MongoDB\Driver\Exception\Exception $e) {
+    echo("Codice  errore".$e->getMessage()."<br>");
+	}
+
 if(isset($_POST['send_specievegetale'])) {
   $timestamp=date("Y-m-d H:i:s");
   $nome=$_SESSION['nome'];
