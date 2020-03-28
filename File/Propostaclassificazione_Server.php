079aacb063889f8fb215a1837d49fe10829f31bd
alesasha21 <62480648+alesasha21@users.noreply.github.com>
2020-03-28 20:00:24 +0100

    Update Propostaclassificazione_Server.php

--- File/Propostaclassificazione_Server.php
+++ File/Propostaclassificazione_Server.php
@@ -2,6 +2,18 @@
 
 session_start();
 
+try {
+     $mng = new MongoDB\Driver\Manager("mongodb://localhost:27017");
+     $bulk = new MongoDB\Driver\BulkWrite();
+     if($_POST){
+     $doc = ['_id' => new MongoDB\BSON\ObjectID(),'nome'=>$_SESSION['nome'],'avvistamento'=>$_POST['avvistamento'], 'commento'=>$_POST['commento'], 'Nomelatino'=>$_POST['nomeLatino'], 'password'=>$_POST['password'],'azione' => 'Nuova proposta classificazione'];
+     $bulk->insert($doc);
+     $mng->executeBulkWrite('Naturae.nat', $bulk);
+   }
+     } catch (MongoDB\Driver\Exception\Exception $e) {
+    echo("Codice  errore".$e->getMessage()."<br>");
+	}
+
 if(isset($_POST['send_proposta'])){
 	$nome=$_SESSION['nome'];
 	$avvistamento=$_POST['avvistamento'];
