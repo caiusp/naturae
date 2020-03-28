65b2d83010e45dd3b55d4729d3aaf914141c32da
alesasha21 <62480648+alesasha21@users.noreply.github.com>
2020-03-28 19:58:34 +0100

    Update Iscrizioneescursione_Server.php

--- File/Iscrizioneescursione_Server.php
+++ File/Iscrizioneescursione_Server.php
@@ -1,6 +1,19 @@
 <?php
 session_start();
 
+try {
+     $mng = new MongoDB\Driver\Manager("mongodb://localhost:27017");
+     $bulk = new MongoDB\Driver\BulkWrite();
+     if($_POST){
+     $doc = ['_id' => new MongoDB\BSON\ObjectID(),'nome'=>$_SESSION['nome'],'codiceEscursione'=>$_POST['id'],'azione' => 'Nuovo iscrizione escursione'];
+     $bulk->insert($doc);
+     $mng->executeBulkWrite('Naturae.nat', $bulk);
+   }
+     } catch (MongoDB\Driver\Exception\Exception $e) {
+    echo("Codice  errore".$e->getMessage()."<br>");
+	}
+
+
 if(isset($_POST['send_iscrizioneEscursione'])) {
 	$id=$_POST['id'];
 	$nome=$_SESSION['nome'];
